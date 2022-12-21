f4ae503a5adfea14aa0f3c476a060a1b22fa4643
PatrykGwardzik <gwardzikpatryk@gmail.com>
2022-12-21 23:06:00 +0100

    Revert "Jason dependencis added to maven Pom file"
    This reverts commit fc2f2a28ec3de571034c6c4f6888e42232a864c1.

--- VacationPlanner-ConsoleApp/src/main/java/com/isa/App.java
+++ VacationPlanner-ConsoleApp/src/main/java/com/isa/App.java
@@ -1,10 +1,6 @@
 package com.isa;
 
 import com.isa.menu.MenuBehaviour;
-import com.isa.model.Employee;
-import com.fasterxml.jackson.core.JacksonException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App
 {
@@ -14,8 +10,5 @@ public class App
 
         MenuBehaviour menu = new MenuBehaviour();
         menu.retrieveSelection();
-
-
-
     }
 }
--- VacationPlanner-ConsoleApp/src/main/java/com/isa/fileManagment/dataManager.java
+++
@@ -1,4 +0,0 @@
-package com.isa.fileManagment;
-
-public class dataManager {
-}
